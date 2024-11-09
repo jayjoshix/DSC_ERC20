46449b0465fab5aaa91a9e2f3c3ed0cdacd1ec72
jay <j.joshi128@gmail.com>
2024-11-09 10:53:26 +0530

    first commit

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-
-contract DecentralizedStableCoin is ERC20Burnable{
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+contract DecentralizedStableCoin is ERC20Burnable,Ownable {
     constructor() ERC20("DecentralizedStableCoin", "DSC"){}
 
-    
+    function burn(uint256 _amount) public override onlyOwner() 
     
 }
